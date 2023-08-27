058d3ce0ab1a3e368996990f363b46034d90ef4c
Melissa Benard <benardmeli@yahoo.fr>
2023-08-27 18:36:45 +0200

    Test the route for an admin

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * Test the authentification of the admin
+     */
+    public function testAccessAdminWithGuestRole(): void
+    {
+        $response = $this->get('/admin/articles');
+        $response->assertRedirect('/login');
+    }
+
+    /**
+     * Test that the user has a admin role
+     */
+    public function testAccessAdminWithAdminRole()
+    {
+        $admin = Auth::loginUsingId(1);
+        //dd($admin);
+        $this->actingAs($admin);
+
+        $response = $this->get('/admin/articles');
+        dd($response->getStatusCode());
+    }
+
+}
